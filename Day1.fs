c7ca796f0c0b6bdd8d799c3c240f9575553e2481
Floyd May <floyd@canyon-trail.com>
2022-12-01 10:56:52 -0600

    day 1 complete

---
+++ Day1.fs
@@ -0,0 +1,39 @@
+﻿module aoc_2022.Day1
+open System.IO
+open FParsec
+
+let private nl = pchar '\n'
+
+let private pElf =
+    manyTill (pint32 .>> nl) nl
+
+let pElves =
+    let fileLines = File.ReadAllLines "./day1.txt"
+    let fileContents = System.String.Join ("\n", fileLines)
+    
+    match run (manyTill pElf eof) fileContents with
+    | Success (result, _, _) -> result
+    | Failure (errorMsg, _, _) -> failwithf $"parse fail: %s{errorMsg}"
+
+let part1 =
+    let elves = pElves
+        
+    let topCalorieElf =
+        elves
+        |> List.map List.sum
+        |> List.sortDescending
+        |> List.head
+    
+    topCalorieElf |> string
+
+let part2 =
+    let elves = pElves
+    
+    let topThreeElves = 
+        elves
+        |> List.map List.sum
+        |> List.sortDescending
+        |> List.take 3
+        |> List.sum
+    
+    topThreeElves |> string
---
+++ Program.fs
@@ -0,0 +1,4 @@
+﻿open aoc_2022
+
+printfn $"Day 1, part 1: %s{Day1.part1}"
+printfn $"Day 1, part 2: %s{Day1.part2}"
